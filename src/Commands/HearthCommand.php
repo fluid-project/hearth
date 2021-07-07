bc5ddbe9c0cf8708376d75175b9367a3e515d5a3
Ned Zimmerman <ned@bight.dev>
2021-07-07 11:05:50 -0300

    feat: add actions and model to stubs, copy

--- src/Commands/HearthCommand.php
+++ src/Commands/HearthCommand.php
@@ -22,5 +22,25 @@ class HearthCommand extends Command
             '--provider' => 'ChinLeung\MultilingualRoutes\MultilingualRoutesServiceProvider',
             '--tag' => 'config',
         ]);
+
+        // AuthenticateSession Middleware...
+        $this->replaceInFile(
+            '// \Illuminate\Session\Middleware\AuthenticateSession::class,',
+            <<<EOT
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \ChinLeung\MultilingualRoutes\DetectRequestLocale::class,
+            EOT,
+            app_path('Http/Kernel.php')
+        );
+
+        (new Filesystem())->ensureDirectoryExists(app_path('Actions/Fortify'));
+
+        copy(__DIR__.'/../../stubs/app/Actions/Fortify/CreateNewUser.php', app_path('Actions/Fortify/CreateNewUser.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Fortify/PasswordValidationRules.php', app_path('Actions/Fortify/PasswordValidationRules.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Fortify/ResetUserPassword.php', app_path('Actions/Fortify/ResetUserPassword.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Fortify/UpdateUserPassword.php', app_path('Actions/Fortify/UpdateUserPassword.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Fortify/UpdateUserProfileInformation.php', app_path('Actions/Fortify/UpdateUserProfileInformation.php'));
+
+        copy(__DIR__.'/../../stubs/app/Models/User.php', app_path('Models/User.php'));
     }
 }
---
+++ stubs/app/Models/User.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Profile;
+use App\Models\Membership;
+use App\Models\Organization;
+use App\Models\Entity;
+use App\Notifications\VerifyEmailNotification;
+use App\Notifications\ResetPasswordNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Translation\HasLocalePreference;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphedByMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use ShiftOneLabs\LaravelCascadeDeletes\CascadesDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class User extends Authenticatable implements HasLocalePreference, MustVerifyEmail
+{
+    use CascadesDeletes;
+    use HasFactory;
+    use HasSlug;
+    use HasTranslations;
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'locale',
+        'theme'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The attributes that are translatable.
+     *
+     * @var array
+     */
+    public $translatable = [];
+
+    protected $cascadeDeletes = ['organizations'];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    /**
+     * Get the user's preferred locale.
+     *
+     * @return string
+     */
+    public function preferredLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Get the consultant profile associated with the user.
+     */
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    /**
+     * Get the user's memberships.
+     */
+    public function memberships(): HasMany
+    {
+        return $this->hasMany(Membership::class);
+    }
+
+    /**
+     * Determine if the user is a member of a given memberable.
+     *
+     * @param mixed $memberable
+     * @return bool
+     */
+    public function isMemberOf($memberable)
+    {
+        return $memberable->hasUserWithEmail($this->email);
+    }
+
+    /**
+     * Determine if the user is an administrator of a given memberable.
+     *
+     * @param mixed $memberable
+     * @return bool
+     */
+    public function isAdministratorOf($memberable)
+    {
+        return $memberable->hasAdministratorWithEmail($this->email);
+    }
+}
