6b9dca1f13fed5c0139925a56ccbaea70473a06f
Ned Zimmerman <ned@bight.dev>
2021-07-12 11:16:35 -0300

    feat: add web routes for users

---
+++ stubs/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::redirect('/', locale());
+Route::multilingual('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::multilingual('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified:' . locale() . '.verification.notice'])->name('dashboard');
+
+Route::multilingual('/account/edit', [UserController::class, 'edit'])
+    ->middleware(['auth'])
+    ->name('users.edit');
+
+Route::multilingual('/account/admin', [UserController::class, 'admin'])
+    ->middleware(['auth'])
+    ->name('users.admin');
+
+Route::multilingual('/account/delete', [UserController::class, 'destroy'])
+    ->method('delete')
+    ->middleware(['auth'])
+    ->name('users.destroy');
+
+require __DIR__ . '/fortify.php';
