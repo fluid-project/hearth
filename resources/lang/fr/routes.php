06b7dbdc47018ef4ee9c38767f715a86d931d833
Ned Zimmerman <ned@bight.dev>
2021-07-15 18:06:02 -0300

    chore(l10n): translate routes.php into French, Canada

--- resources/lang/fr/routes.php
+++ resources/lang/fr/routes.php
@@ -1,3 +1,9 @@
 <?php
 
-return [];
+return [
+    'dashboard' => 'dashboard',
+    'logout' => 'logout',
+    'login' => 'login',
+    'register' => 'register',
+    'verification.verify' => '/verify-email/{id}/{hash}'
+];
