e2ffea2232c4d7dab511d1a28f5b5dc5557a24d6
Ned Zimmerman <ned@bight.dev>
2021-07-15 17:52:48 -0300

    New translations auth.php (French, Canada)

---
+++ fr-CA/resources/lang/fr/auth.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'sign_in' => 'S’identifier',
+    'sign_out' => 'Se déconnecter',
+    'create_account' => 'Créer un compte',
+    'label_password' => 'Mot de passe',
+    'label_password_confirmation' => 'Confirmez le mot de passe',
+    'existing_account_prompt' => 'Vous avez déjà un compte?',
+    'create_your_account' => 'Créez votre compte',
+    'label_current_password' => 'Mot de passe actuel',
+    'change_password' => 'Changer le mot de passe',
+    'action_confirm' => 'Confirmer',
+    'password_change_succeeded' => 'Votre mot de passe a été modifié.',
+    'label_remember_me' => 'Souvenez-vous de moi',
+    'error_intro' => 'Désolé, quelque chose s’est mal passé.',
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'wrong_password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+    'confirm_intro' => 'Pour votre sécurité, veuillez confirmer votre mot de passe avant de continuer.',
+    'forget_prompt' => 'Mot de passe oublié?',
+    'forgot_intro' => 'Mot de passe oublié? Entrez votre adresse e-mail et nous vous enverrons par e-mail un lien de réinitialisation du mot de passe qui vous permettra d’en choisir un nouveau.',
+    'forgot_submit' => 'Envoyer le lien de réinitialisation',
+    'reset_submit' => 'Réinitialiser le mot de passe',
+    'verification_required' => 'Vérification de l\'e-mail requise',
+    'verification_intro' => 'Veuillez vérifier votre adresse e-mail en cliquant sur le lien que nous vous avons envoyé. Si vous n\'avez pas reçu le courriel, nous vous enverrons volontiers un autre.',
+    'verification_sent' => 'Un nouveau lien de vérification a été envoyé à l\'adresse e-mail que vous avez fournie.',
+    'resend_verification_email' => 'Renvoyer l’e-mail de vérification',
+    'verification_succeeded' => 'Votre adresse e-mail a été vérifiée.'
+];
