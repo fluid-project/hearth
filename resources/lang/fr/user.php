d573951ffcb9b7130ae8345b97c8660a5fa2e8bd
Ned Zimmerman <ned@bight.dev>
2021-07-15 18:06:03 -0300

    chore(l10n): translate user.php into French, Canada

--- resources/lang/fr/user.php
+++ resources/lang/fr/user.php
@@ -1,3 +1,13 @@
 <?php
 
-return [];
+return [
+    'settings' => 'Settings',
+    'account' => 'Account',
+    'label_name' => 'Full name',
+    'label_locale' => 'Preferred language',
+    'delete_account' => 'Delete account',
+    'delete_account_intro' => 'Your account will be deleted and cannot be recovered. If you still want to delete your account, please enter your current password to proceed.',
+    'action_delete_account' => 'Delete my account',
+    'settings_update_succeeded' => 'Your settings have been saved.',
+    'destroy_succeeded' => 'Your account has been deleted.',
+];
