e0d664da380310f4e5e136eabb13bc2ccedcca0f
Ned Zimmerman <ned@bight.dev>
2021-07-15 18:06:00 -0300

    chore(l10n): translate forms.php into French, Canada

--- resources/lang/fr/forms.php
+++ resources/lang/fr/forms.php
@@ -1,3 +1,8 @@
 <?php
 
-return [];
+return [
+    'save_changes' => 'Save changes',
+    'label_email' => 'Email address',
+    'errors_found' => 'Errors found',
+    'errors_found_message' => 'Sorry, some errors were found in your submission. Please correct these errors and try again.'
+];
