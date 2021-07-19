933c178b2cfb0ca4eea5c153d025a0c24c9084a0
Ned Zimmerman <ned@bight.dev>
2021-07-19 11:36:20 -0300

    chore(l10n): translate passwords.php into French, Canada

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Nous avons envoyé votre lien de réinitialisation de mot de passe!',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne pouvons pas trouver un utilisateur avec cette adresse e-mail.",
 
 ];
