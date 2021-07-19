4d5dfdb8878a91b6538969c73ab59d41cccb29c9
Ned Zimmerman <ned@bight.dev>
2021-07-19 11:36:22 -0300

    chore(l10n): translate validation.php into French, Canada

--- resources/lang/fr/validation.php
+++ resources/lang/fr/validation.php
@@ -2,13 +2,13 @@
 
 return [
     'password' => [
-        'length-uppercase' => 'The :attribute must be at least :length characters and contain at least one uppercase character.',
-        'length-numeric' => 'The :attribute must be at least :length characters and contain at least one number.',
-        'length-specialcharacter' => 'The :attribute must be at least :length characters and contain at least one special character.',
-        'length-uppercase-numeric' => 'The :attribute must be at least :length characters and contain at least one uppercase character and one number.',
-        'length-uppercase-specialcharacter' => 'The :attribute must be at least :length characters and contain at least one uppercase character and one special character.',
-        'length-uppercase-numeric-specialcharacter' => 'The :attribute must be at least :length characters and contain at least one uppercase character, one number, and one special character.',
-        'length-numeric-specialcharacter' => 'The :attribute must be at least :length characters and contain at least one special character and one number.',
-        'length' => 'The :attribute must be at least :length characters.'
+        'length-uppercase' => 'Le champ :attribute doit comporter au moins :length caractères et contenir au moins un caractère majuscule.',
+        'length-numeric' => 'Le champ :attribute doit comporter au moins :length caractères et contenir au moins un chiffre.',
+        'length-specialcharacter' => 'Le champ :attribute doit comporter au moins :length caractères et contenir au moins un caractère spécial.',
+        'length-uppercase-numeric' => 'Le champ :attribute doit comporter au moins :length caractères et contenir au moins un caractère majuscule et un chiffre.',
+        'length-uppercase-specialcharacter' => 'Le champ :attribute doit comporter au moins :length caractères et contenir au moins un caractère majuscule et un caractère spécial.',
+        'length-uppercase-numeric-specialcharacter' => 'Le champ :attribute doit comporter au moins :length caractères et contenir au moins un caractère majuscule, un chiffre et un caractère spécial.',
+        'length-numeric-specialcharacter' => 'Le champ :attribute doit comporter au moins :length caractères et contenir au moins un caractère spécial et un chiffre.',
+        'length' => 'Le texte :attribute doit contenir au moins :min caractères.'
     ]
 ];
