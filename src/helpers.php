573a1d6d00ac4c38700d2e4c501605a0e3a10329
Ned Zimmerman <ned@bight.dev>
2021-07-29 14:53:30 -0300

    fix: update get_region_name() to match method in commerceguys/addressing

--- src/helpers.php
+++ src/helpers.php
@@ -2,20 +2,27 @@
 
 use CommerceGuys\Addressing\Subdivision\SubdivisionRepository;
 
+$subdivisionRepository = new SubdivisionRepository();
+
+$subdivision = $subdivisionRepository->get('CA-ON', ["CA"]);
+
+ray($subdivision);
+
 if (! function_exists('get_region_name')) {
     /**
      * Get the name of an administrative subdivision from its code.
      *
-     * @param string $code An ISO 3166-2 administrative subdivision code.
+     * @param string $code An administrative subdivision code.
+     * @param array $countries An array of ISO 3166-1 alpha-2 country codes.
      * @param string $locale An ISO 639-1 language code.
      *
      * @return string The name of the administrative subdivision.
      */
-    function get_region_name($code, $country = 'CA', $locale = 'en')
+    function get_region_name($code, $countries = ['CA'], $locale = 'en')
     {
         $subdivisionRepository = new SubdivisionRepository();
 
-        $subdivision = $subdivisionRepository->get("{$country}-{$code}");
+        $subdivision = $subdivisionRepository->get($code, $countries);
 
         return ($locale === $subdivision->getLocale()) ? $subdivision->getLocalName() : $subdivision->getName();
     }
@@ -28,7 +35,7 @@ if (! function_exists('get_regions')) {
      * @param array $countries An array of ISO 3166-1 alpha-2 country codes.
      * @param string $locale An ISO 639-1 language code.
      *
-     * @return array An array of administrative subdivision names keyed by ISO 3166-2 administrative subdivision codes.
+     * @return array An array of administrative subdivision names keyed by administrative subdivision codes.
      */
     function get_regions($countries = ['CA'], $locale = 'en')
     {
@@ -37,7 +44,7 @@ if (! function_exists('get_regions')) {
         $regions = [];
 
         foreach ($subdivisionRepository->getAll($countries) as $region) {
-            $regions[$region->getIsoCode()] = ($locale === $region->getLocale()) ? $region->getLocalName() : $region->getName();
+            $regions[$region->getCode()] = ($locale === $region->getLocale()) ? $region->getLocalName() : $region->getName();
         }
 
         return $regions;
@@ -50,7 +57,7 @@ if (! function_exists('get_region_codes')) {
      *
      * @param array $countries An array of ISO 3166-1 alpha-2 country codes.
      *
-     * @return array An array of ISO 3166-2 administrative subdivision codes.
+     * @return array An array of administrative subdivision codes.
      */
     function get_region_codes($countries = ['CA'])
     {
@@ -59,7 +66,7 @@ if (! function_exists('get_region_codes')) {
         $regions = [];
 
         foreach ($subdivisionRepository->getAll($countries) as $region) {
-            $regions[] = $region->getIsoCode();
+            $regions[] = $region->getCode();
         }
 
         return $regions;
