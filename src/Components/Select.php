67cacb4155f76159956c14119ff4109df8f324fe
Ned Zimmerman <ned@bight.dev>
2021-08-25 08:45:17 -0300

    fix: add describedBy method to Select component

--- src/Components/Select.php
+++ src/Components/Select.php
@@ -123,6 +123,26 @@ class Select extends Component
     }
 
     /**
+     * Generate the aria-describedby attribute for the form input.
+     *
+     * @return string
+     */
+    public function describedBy()
+    {
+        $descriptors = [];
+
+        if ($this->hinted) {
+            $descriptors[] = $this->name . '-hint';
+        }
+
+        if ($this->invalid) {
+            $descriptors[] = $this->name . '-error';
+        }
+
+        return implode(' ', $descriptors);
+    }
+
+    /**
      * Get the view / contents that represent the component.
      *
      * @return \Illuminate\View\View
