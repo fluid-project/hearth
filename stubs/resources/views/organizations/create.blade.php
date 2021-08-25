f65f0f88f5b3070a63f901787d8145547e0b5788
Ned Zimmerman <ned@bight.dev>
2021-08-24 22:09:05 -0300

    fix: adjust error class

--- stubs/resources/views/organizations/create.blade.php
+++ stubs/resources/views/organizations/create.blade.php
@@ -13,17 +13,17 @@
         @csrf
         <x-hearth-input id="user_id" type="hidden" name="user_id" :value="Auth::user()->id" required />
 
-        <div class="field @error('name') field-error @enderror">
+        <div class="field @error('name') field--error @enderror">
             <x-hearth-label for="name" :value="__('organization.label_name')" />
             <x-hearth-input id="name" type="text" name="name" required />
             <x-hearth-error for="name" />
             </div>
-        <div class="field @error('locality') field-error @enderror">
+        <div class="field @error('locality') field--error @enderror">
             <x-hearth-label for="locality" :value="__('forms.label_locality')" />
             <x-hearth-input id="locality" type="text" name="locality" required />
             <x-hearth-error for="locality" />
         </div>
-        <div class="field @error('region') field-error @enderror">
+        <div class="field @error('region') field--error @enderror">
             <x-hearth-label for="region" :value="__('forms.label_region')" />
             <x-hearth-select id="region" name="region" required :options="$regions" />
             <x-hearth-error for="region" />
--- stubs/resources/views/organizations/edit.blade.php
+++ stubs/resources/views/organizations/edit.blade.php
@@ -13,17 +13,17 @@
         @csrf
         @method('PUT')
 
-        <div class="field @error('name') field-error @enderror">
+        <div class="field @error('name') field--error @enderror">
             <x-hearth-label for="name" :value="__('organization.label_name')" />
             <x-hearth-input id="name" type="text" name="name" :value="old('name', $organization->name)" required />
             <x-hearth-error for="name" />
         </div>
-        <div class="field @error('locality') field-error @enderror">
+        <div class="field @error('locality') field--error @enderror">
             <x-hearth-label for="locality" :value="__('forms.label_locality')" />
             <x-hearth-input id="locality" type="text" name="locality" :value="old('locality', $organization->locality)" required />
             <x-hearth-error for="locality" />
         </div>
-        <div class="field @error('region') field-error @enderror">
+        <div class="field @error('region') field--error @enderror">
             <x-hearth-label for="region" :value="__('forms.label_region')" />
             <x-hearth-select id="region" name="region" :selected="old('region', $organization->region)" required :options="$regions"/>
             <x-hearth-error for="region" />
