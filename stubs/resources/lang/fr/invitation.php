78cee256e520e45ef25cf0cfda736fc0664a488c
Ned Zimmerman <ned@bight.dev>
2021-08-25 21:37:24 -0300

    chore(l10n): update strings (#42)
    * feat: improve error handling for invitation flow (resolves #28)
    
    * chore(l10n): update strings

--- stubs/resources/lang/fr/invitation.php
+++ stubs/resources/lang/fr/invitation.php
@@ -9,7 +9,7 @@ return [
     'create_invitation_succeeded' => 'Your invitation has been sent.',
     'invited_user_already_belongs_to_team' => 'This user already belongs to this team.',
     'cancel_invitation_succeeded' => 'The invitation has been cancelled.',
-    'email_not_valid' => 'You are logged in as :email, but this invitation belongs to a different user.',
+    'email_not_valid' => 'You are logged in as :email, but this invitation is for a different email address.',
     'accept_invitation_succeeded' => 'You have joined the :inviteable team.',
     'invitations_title' => 'Member invitations',
     'invitation_email' => 'Email',
