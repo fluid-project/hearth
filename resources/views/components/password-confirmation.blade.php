892fbb2adc7aad9df0c0337a43b104d4a2de3bb1
Ned Zimmerman <ned@bight.dev>
2021-08-30 14:44:58 -0300

    feat: replace validation-error component with hearth-error (#56)
    feat: add slot support to hearth-error component

--- resources/views/components/password-confirmation.blade.php
+++ resources/views/components/password-confirmation.blade.php
@@ -4,11 +4,11 @@
         <div class="modal flow" @keydown.escape.window="hideModal()" @click.away="hideModal()">
             <p>{{ $message }}</p>
 
-            <div class="field">
+            <div x-bind:class="validationError ? 'field field--error' : 'field'">
                 <x-hearth-label for="password" :value="__('hearth::auth.label_password')" />
-                <x-hearth-input id="password" type="password" name="password" required x-ref="password" />
+                <x-hearth-input id="password" type="password" name="password" required x-ref="password" x-bind:ariaD-describedby="validationError ? 'password-error' : ''"/>
                 <template x-cloak x-if="validationError">
-                    <x-validation-error>{{ __('validation.current_password') }}</x-validation-error>
+                    <x-hearth-error for="password">{{ __('validation.current_password') }}</x-hearth-error>
                 </template>
             </div>
 
