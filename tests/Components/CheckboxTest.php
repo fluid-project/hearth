72b6466fd6d1ba311228240b3550d14e1c0cde96
Ned Zimmerman <ned@bight.dev>
2021-11-18 11:15:35 -0400

    test: add tests for Checkbox component

---
+++ tests/Components/CheckboxTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Hearth\Tests\Components;
+
+use Hearth\Components\Checkbox;
+use Hearth\Tests\TestCase;
+
+class CheckboxTest extends TestCase
+{
+    public function test_checkbox_component_renders()
+    {
+        $view = $this->withViewErrors([])
+            ->component(
+                Checkbox::class,
+                [
+                    'name' => 'remember',
+                ],
+            );
+
+        $view->assertSee('id="remember"', false);
+    }
+
+    public function test_checkbox_component_references_hint()
+    {
+        $view = $this->withViewErrors([])
+            ->component(
+                Checkbox::class,
+                [
+                    'name' => 'remember',
+                    'hinted' => true,
+                ],
+            );
+
+        $view->assertSee('aria-describedby="remember-hint"', false);
+    }
+
+    public function test_checkbox_component_references_custom_hint()
+    {
+        $view = $this->withViewErrors([])
+            ->component(
+                Checkbox::class,
+                [
+                    'name' => 'remember',
+                    'hinted' => 'remember-login-hint',
+                ],
+            );
+
+        $view->assertSee('aria-describedby="remember-login-hint"', false);
+    }
+
+    public function test_checkbox_component_includes_attribute()
+    {
+        $view = $this->withViewErrors([])
+            ->blade(
+                '<x-hearth-checkbox :name="$name" x-model="remember" />',
+                [
+                    'name' => 'remember',
+                ],
+            );
+
+        $view->assertSee('x-model="remember"', false);
+    }
+}
