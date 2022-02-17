e8b081778ad70fe1bee0fb66b4acdfc68c2cdc48
greatislander <greatislander@users.noreply.github.com>
2022-02-17 15:20:38 +0000

    fix: apply code styling rules

--- stubs/app/Http/Controllers/ProfileInformationController.php
+++ stubs/app/Http/Controllers/ProfileInformationController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
 
 class ProfileInformationController extends Controller
@@ -16,8 +15,10 @@ class ProfileInformationController extends Controller
      * @param  \Laravel\Fortify\Contracts\UpdatesUserProfileInformation  $updater
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request,
-                           UpdatesUserProfileInformation $updater)
+    public function update(
+        Request $request,
+        UpdatesUserProfileInformation $updater
+    )
     {
         $updater->update($request->user(), $request->all());
 
