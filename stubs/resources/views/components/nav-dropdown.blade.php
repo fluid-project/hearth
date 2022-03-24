f2654e7434c0bce6d52bcb0a9ea49f284e88466d
Ned Zimmerman <ned@bight.dev>
2022-03-24 10:27:27 -0300

    fix: replace Alpine .away with .outside

--- stubs/resources/views/components/nav-dropdown.blade.php
+++ stubs/resources/views/components/nav-dropdown.blade.php
@@ -1,4 +1,4 @@
-<li {{ $attributes->merge(['class' => 'dropdown']) }} x-data="{ open: false }" @keyup.escape.window="open = false" @click.away="open = false" @close.stop="open = false">
+<li {{ $attributes->merge(['class' => 'dropdown']) }} x-data="{ open: false }" @keyup.escape.window="open = false" @click.outside="open = false" @close.stop="open = false">
     <button class="link" @click="open = ! open" x-bind:aria-expanded="open.toString()">
         {{ $trigger }}
     </button>
