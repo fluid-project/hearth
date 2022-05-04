84ec2e566cf1bf7a5a8e8ac0fe0be3243fd2ab1d
Justin Obara <jobara@users.noreply.github.com>
2022-05-04 12:35:08 -0400

    style: remove whitespace that caused issue with PHP-CS-Fixer
    Co-authored-by: Ned Zimmerman <ned@bight.dev>

--- stubs/app/Http/Requests/UpdateOrganizationRequest.php
+++ stubs/app/Http/Requests/UpdateOrganizationRequest.php
@@ -7,9 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class UpdateOrganizationRequest extends FormRequest
-{
-
-
+ {
     /**
      * Determine if the user is authorized to make this request.
      *
