1c5946684f5c0e0443b43dbe47344553f2aba541
Ned Zimmerman <ned@bight.dev>
2022-05-04 15:24:48 -0300

    fix: resolve issue with button type not being overridden (fix #121)

--- src/Components/Button.php
+++ src/Components/Button.php
@@ -19,7 +19,7 @@ class Button extends Component
      *
      * @return void
      */
-    public function __construct($type = "submit")
+    public function __construct($type = 'submit')
     {
         $this->type = $type;
     }
