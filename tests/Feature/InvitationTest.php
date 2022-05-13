3573bd8ddc132a9c850f86dc2fe043e74e63c214
Ned Zimmerman <ned@bight.dev>
2022-05-13 17:14:45 -0300

    test: add Invitation test

---
+++ tests/Feature/InvitationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Hearth\Tests\Feature;
+
+use Hearth\Models\Invitation;
+use Hearth\Models\Membership;
+use Hearth\Tests\Fixtures\Organization;
+use Hearth\Tests\Fixtures\User;
+use Hearth\Tests\TestCase;
+use Illuminate\Support\Facades\Schema;
+
+class InvitationTest extends TestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        Schema::dropAllTables();
+
+        $create_users_table = include __DIR__ . '/../../database/migrations/create_users_table.php.stub';
+        $update_users_table = include __DIR__ . '/../../database/migrations/update_users_table.php.stub';
+        $create_invitations_table = include __DIR__ . '/../../database/migrations/2021_03_01_000000_create_invitations_table.php';
+        $create_organizations_table = include __DIR__ . '/../../database/migrations/create_organizations_table.php.stub';
+
+        $create_users_table->up();
+        $update_users_table->up();
+        $create_invitations_table->up();
+        $create_organizations_table->up();
+    }
+
+    public function test_invitation_can_be_created_for_organization()
+    {
+        $user = User::forceCreate([
+            'name' => 'Frodo Baggins',
+            'email' => 'frodo@bag-end.net',
+            'password' => 'secret',
+        ]);
+
+        $organization = Organization::forceCreate([
+            'name' => json_encode(['en' => 'Fellowship']),
+            'locality' => 'Rivendell',
+            'region' => 'BC',
+        ]);
+
+        $invitation = Invitation::forceCreate([
+            'invitationable_id' => $organization->id,
+            'invitationable_type' => get_class($organization),
+            'email' => $user->email,
+            'role' => 'admin',
+        ]);
+
+        $this->assertEquals($organization->id, $invitation->invitationable->id);
+    }
+}
