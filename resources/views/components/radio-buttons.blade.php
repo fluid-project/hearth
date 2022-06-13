ddc828dc2aa5660a88e399fd086e2096de945375
Ned Zimmerman <ned@bight.dev>
2022-06-13 15:15:53 -0300

    fix: don't use strict comparison for radio buttons

--- resources/views/components/radio-buttons.blade.php
+++ resources/views/components/radio-buttons.blade.php
@@ -1,7 +1,7 @@
 @foreach($options as $value => $atts)
 <div class="field">
     @php($hint = isset($atts['hint']) ? $name . '-' . $value . '-hint' : '')
-    <input {!! $attributes !!} type="radio" name="{{ $name }}" id="{{ $name }}-{{ $value }}" value="{{ $value }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked($checked === $value) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
+    <input {!! $attributes !!} type="radio" name="{{ $name }}" id="{{ $name }}-{{ $value }}" value="{{ $value }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked($checked == $value) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
     <x-hearth-label for="{{ $name }}-{{ $value }}">{{ $atts['label'] }}</x-hearth-label>
     @if(isset($atts['hint']))
     <x-hearth-hint for="{{ $name }}-{{ $value }}">{{ $atts['hint'] }}</x-hearth-hint>
