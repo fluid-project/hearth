a908a8baafa7f34cd691d0f81a59878d631ed743
Daniel Cho <chosww@gmail.com>
2022-06-24 15:36:16 -0400

    fix: update test cases for translatable components

--- tests/Components/TranslatableTextareaTest.php
+++ tests/Components/TranslatableTextareaTest.php
@@ -2,16 +2,39 @@
 
 namespace Hearth\Tests\Components;
 
-use Hearth\Components\TranslatableTextarea;
 use Hearth\Tests\TestCase;
+use Illuminate\Support\Facades\App;
 
 class TranslatableTextareaTest extends TestCase
 {
-    public function test_translatable_textarea_component_renders()
+    public function test_translatable_textarea_component_render_in_english()
     {
         $view = $this->withViewErrors([])
-            ->component(
-                TranslatableTextarea::class,
+        ->blade(
+            '<x-hearth-translatable-textarea :name="$name" :label="$label" :locales="$locales" :model="$model"> </x-hearth-translatable-textarea>',
+            [
+                'name' => 'resource',
+                'label' => 'test resource',
+                'locales' => ['en', 'fr'],
+                'model' => null,
+            ],
+        );
+
+        $view->assertSee('for="resource_en"', false);
+        $view->assertSee('name="resource[en]"', false);
+        $view->assertSee('id="resource_en"', false);
+        $view->assertSee('id="test-resource-french"', false);
+        $view->assertSee('id="resource.en-error', false);
+        $view->assertSeeText('test resource (English)', false);
+        $view->assertSeeText('test resource (French)', false);
+    }
+
+    public function test_translatable_textarea_component_render_in_french()
+    {
+        App::setLocale('fr');
+        $view = $this->withViewErrors([])
+            ->blade(
+                '<x-hearth-translatable-textarea :name="$name" :label="$label" :locales="$locales" :model="$model"> </x-hearth-translatable-input>',
                 [
                     'name' => 'resource',
                     'label' => 'test resource',
@@ -19,10 +42,12 @@ class TranslatableTextareaTest extends TestCase
                     'model' => null,
                 ],
             );
-
-        $view->assertSee('for="resource_en"', false);
-        $view->assertSee('name="resource[en]"', false);
-        $view->assertSee('id="resource_en"', false);
-        $view->assertSee('id="test-resource-french"', false);
+        $view->assertSee('for="resource_fr"', false);
+        $view->assertSee('name="resource[fr]"', false);
+        $view->assertSee('id="resource_fr"', false);
+        $view->assertSee('id="test-resource-english"', false);
+        $view->assertSee('id="resource.fr-error', false);
+        $view->assertSeeText('test resource (French)', false);
+        $view->assertSeeText('test resource (English)', false);
     }
 }
