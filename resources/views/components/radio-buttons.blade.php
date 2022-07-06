a2bf25633571651100c8b47fa9dcc1e117b205e4
Ned Zimmerman <ned@bight.dev>
2022-07-06 14:44:04 -0600

    feat: add unique ID to checkboxes and radio buttons

--- resources/views/components/radio-buttons.blade.php
+++ resources/views/components/radio-buttons.blade.php
@@ -1,5 +1,5 @@
 @foreach($options as $value => $atts)
-<div class="field">
+<div class="field" id="{{ $name }}">
     @php($hint = isset($atts['hint']) ? $name . '-' . $value . '-hint' : '')
     <input {!! $attributes !!} type="radio" name="{{ $name }}" id="{{ $name }}-{{ $value }}" value="{{ $value }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked($checked == $value) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
     <x-hearth-label for="{{ $name }}-{{ $value }}">{{ $atts['label'] }}</x-hearth-label>
