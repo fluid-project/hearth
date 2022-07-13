d1faee21f3ba56988efab42b97f9faddf4be52cb
Ned Zimmerman <ned@bight.dev>
2022-07-13 19:21:37 -0300

    fix: simplify further

--- resources/views/components/checkboxes.blade.php
+++ resources/views/components/checkboxes.blade.php
@@ -1,7 +1,7 @@
 @foreach($options as $value => $atts)
 <div class="field">
-    @php($hint = isset($atts['hint']) ? $name . '-' . Str::slug($value) . '-hint' : '')
     @php($id = $name . '-' . Str::slug($value))
+    @php($hint = isset($atts['hint']) ? $id . '-hint' : '')
     <input {!! $attributes !!} type="checkbox" name="{{ $name }}[]" id="{{ $id }}" value="{{ $value }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked(in_array($value, $checked, true)) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
     <x-hearth-label for="{{ $id }}">{{ $atts['label'] }}</x-hearth-label>
     @if(isset($atts['hint']))
--- resources/views/components/radio-buttons.blade.php
+++ resources/views/components/radio-buttons.blade.php
@@ -1,7 +1,7 @@
 @foreach($options as $value => $atts)
 <div class="field">
-    @php($hint = isset($atts['hint']) ? $name . '-' . Str::slug($value) . '-hint' : '')
     @php($id = $name . '-' . Str::slug($value))
+    @php($hint = isset($atts['hint']) ? $id . '-hint' : '')
     <input {!! $attributes !!} type="radio" name="{{ $name }}" id="{{ $id }}" value="{{ $value }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked($checked == $value) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
     <x-hearth-label for="{{ $id }}">{{ $atts['label'] }}</x-hearth-label>
     @if(isset($atts['hint']))
