294bba32b68f296df80914dd760ff8ccf76804ea
Ned Zimmerman <ned@bight.dev>
2022-07-13 12:25:08 -0300

    feat: generate ID in one place

--- resources/views/components/radio-buttons.blade.php
+++ resources/views/components/radio-buttons.blade.php
@@ -1,10 +1,11 @@
 @foreach($options as $value => $atts)
 <div class="field">
     @php($hint = isset($atts['hint']) ? $name . '-' . Str::slug($value) . '-hint' : '')
-    <input {!! $attributes !!} type="radio" name="{{ $name }}" id="{{ $name }}-{{ Str::slug($value) }}" value="{{ $value }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked($checked == $value) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
-    <x-hearth-label for="{{ $name }}-{{ Str::slug($value) }}">{{ $atts['label'] }}</x-hearth-label>
+    @php($id = $name . '-' . Str::slug($value))
+    <input {!! $attributes !!} type="radio" name="{{ $name }}" id="{{ $id }}" value="{{ $value }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked($checked == $value) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
+    <x-hearth-label for="{{ $id }}">{{ $atts['label'] }}</x-hearth-label>
     @if(isset($atts['hint']))
-    <x-hearth-hint for="{{ $name }}-{{ Str::slug($value) }}">{{ $atts['hint'] }}</x-hearth-hint>
+    <x-hearth-hint for="{{ $id }}">{{ $atts['hint'] }}</x-hearth-hint>
     @endif
 </div>
 @endforeach
