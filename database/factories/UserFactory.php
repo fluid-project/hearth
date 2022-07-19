41a5d2ca00737dd92a5b603148a023a5767fe8fb
Ned Zimmerman <ned@bight.dev>
2022-07-19 15:15:46 -0300

    chore: improve UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,12 @@ class UserFactory extends Factory
 
         return [
             'name' => $name,
-            'slug' => Str::slug($name),
-            'email' => $this->faker->unique()->safeEmail(),
+            'slug' => function (array $attributes) {
+                return Str::slug($attributes['name']);
+            },
+            'email' => function (array $attributes) {
+                return Str::slug($attributes['name']).'@'.$this->faker->safeEmailDomain();
+            },
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
