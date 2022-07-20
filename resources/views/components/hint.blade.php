dadcec338efb7cd0f77b693361df8260fb854034
Ned Zimmerman <ned@bight.dev>
2022-07-20 08:43:07 -0300

    feat: use inlineMarkdown method

--- resources/views/components/hint.blade.php
+++ resources/views/components/hint.blade.php
@@ -1,3 +1,1 @@
-<div class="field__hint" id="{{ $for }}-hint">
-    {!! Str::markdown($slot) !!}
-</div>
+<p class="field__hint" id="{{ $for }}-hint">{!! Str::inlineMarkdown($slot) !!}</p>
--- tests/Components/HintTest.php
+++ tests/Components/HintTest.php
@@ -11,13 +11,13 @@ class HintTest extends TestCase
         $view = $this->blade('<x-hearth-hint for="fname">Enter your given name.</x-hearth-hint>');
 
         $view->assertSee('id="fname-hint"', false);
-        $view->assertSee('<p>Enter your given name.</p>', false);
+        $view->assertSee("<p class=\"field__hint\" id=\"fname-hint\">Enter your given name.\n</p>", false);
     }
 
     public function test_hint_component_renders_markdown()
     {
         $view = $this->blade('<x-hearth-hint for="fname">Enter a valid [DOI](https://www.doi.org).</x-hearth-hint>');
 
-        $view->assertSee('<p>Enter a valid <a href="https://www.doi.org">DOI</a>.</p>', false);
+        $view->assertSee("<p class=\"field__hint\" id=\"fname-hint\">Enter a valid <a href=\"https://www.doi.org\">DOI</a>.\n</p>", false);
     }
 }
