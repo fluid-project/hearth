9c4111edfb0533d5c3e4590553d5d7fe6bc6588f
Ned Zimmerman <ned@bight.dev>
2022-07-22 09:26:48 -0300

    fix: restore isset check for hints

--- resources/views/components/checkboxes.blade.php
+++ resources/views/components/checkboxes.blade.php
@@ -4,7 +4,7 @@
     @php($hint = isset($option['hint']) ? $id . '-hint' : '')
     <input {!! $attributes !!} type="checkbox" name="{{ $name }}[]" id="{{ $id }}" value="{{ $option['value'] }}" {!! $describedBy($hint) ? 'aria-describedby="' . $describedBy($hint) . '"' : '' !!} @checked(in_array($option['value'], $checked)) {!! $invalid ? 'aria-invalid="true"' : '' !!} />
     <x-hearth-label for="{{ $id }}">{{ $option['label'] }}</x-hearth-label>
-    @if($option['hint'])
+    @if(isset($option['hint']) && !empty($option['hint']))
     <x-hearth-hint for="{{ $id }}">{{ $option['hint'] }}</x-hearth-hint>
     @endif
 </div>
